2c0013aa279557c2d31cd2bae81f099eaf66e650
Jayachandra134 <reachjayachandra134@gmail.com>
2024-04-18 19:08:24 +0530

    commitrd

---
+++ logfiles.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+userid=$(id -u)
+timestamp=$(date +%F-%H-%M)
+scriptname=$(echo $0 | cut -d "." -f1)
+logfile=/temp/$scriptname-$timestamp.log
+if [ $userid -eq 0 ]
+   then
+       echo "you are a super user"
+    else
+       echo "you are not a super usser"
+       exit
+fi
+
+validate(){
+  if [ $1 -eq 0 ]
+  then
+      echo "$2 :: Done"
+  else
+      echo "$2 :: Failed"
+
+  fi
+}
+
+dnf install mysql -y &>>logfile
+validate $? "sql installation ::"
+echo "$?"
+dnf install git -y &>>logfile
+validate $? "Git installation ::"
+echo "$?"
