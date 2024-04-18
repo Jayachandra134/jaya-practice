69e92074ab583b1ac48612cf86104586f4f2fb2a
Jayachandra134 <reachjayachandra134@gmail.com>
2024-04-18 13:35:08 +0530

    commit1

---
+++ 04-functions.sh
@@ -0,0 +1,24 @@
+#1/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ] 
+then
+    echo "your are not a super user"
+    exit
+ else 
+    echo "you are a super user"
+
+dnf install mysql -y
+
+
+validate(){
+  if [ $1 -ne 0 ]
+      then
+         echo "mysql installation success"
+         exit
+       else
+         echo "$2 is Failed"
+}
+
+validate $? "installing mysql'
