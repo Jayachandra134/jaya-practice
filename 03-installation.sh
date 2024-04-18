33abd17a1c27b6247b58ff9640b716c56eccd059
Jayachandra134 <reachjayachandra134@gmail.com>
2024-04-18 12:23:52 +0530

    commit6

--- 03-installation.sh
+++ 03-installation.sh
@@ -4,10 +4,20 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
    then
        echo "please run this script with super user"
-      # exit 1  #Comes out if error occurs
+       exit 1  #Comes out if error occurs
     else
        echo "you are a super user"
 fi
 
-sudo dnf install mysql -y
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+   then
+       echo "installation of my sql is.....sucess"
+       exit 1
+    else   
+      echo "installation of my sql is...........failed"
+fi
+
+echo "script is proceding"
 
