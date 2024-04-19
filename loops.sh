f7da4dc1fd1048920ede67e0815790475b17d77e
Jayachandra134 <reachjayachandra134@gmail.com>
2024-04-19 12:33:52 +0530

    1commit

---
+++ loops.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in {1..30}
+do
+   echo $i
+   if [ $i -eq 15 ]
+      then 
+           echo "you will get it if work hard"
+      else
+           echo "you have to work too hard to get the job"
+    fi
+Done   
